8f8f3f470af6d0db33bebee5a727a8de4f8506b1
Prasilla <prasillamanchem@gmail.com>
2022-09-19 08:06:17 +0530

    action created to update & delete post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -20,3 +20,21 @@ export const createPost = (post) => async(dispatch) => {
         console.log(error);
      }
 }
+
+export const updatePost = (id, post) => async( dispatch) => {
+    try{
+        const data = await api.updatePost(id, post);
+        dispatch({ type : 'UPDATE', payload : data})
+    }catch(error){
+        console.log(`Update post`, error)
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try{
+        await api.deletePost(id);
+        dispatch( { type : 'DELETE'  , payload : id});
+    }catch(error){
+        console.log(error)
+    }
+}
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000';
+const URL = 'http://localhost:5000/posts';
 
 export const fetchPosts = async () => {
-    let posts =  await axios.get(`${URL}/posts`);
+    let posts =  await axios.get(`${URL}`);
     return posts
 
 }
 
-export const createPost = (newPost) => axios.post(`${URL}/posts`, newPost)
+export const updatePost = (id, updatedPost ) => {
+    return axios.patch(`${URL}/${id}`, updatedPost)
+}
+
+export const createPost = (newPost) => axios.post(`${URL}`, newPost)
+export const deletePost = (id) => axios.delete(`${URL}/${id}`);
+
 
